feeeea714d12a6ba4ee689ef9c00b9aea2f47326
gettingbettersuhani <suhanipareek01@gmail.com>
2025-07-31 23:29:25 +0530

    Update Design.sv

--- Design.sv
+++ Design.sv
@@ -1,1 +1,8 @@
+module full_sub(diff,bout,a,b,bin);
+  input a, b, bin;
+  output diff, bout;
+  assign diff = a ^ b ^ bin ; // XOR of inputs
+  assign bout = (~a & b) | (bin & (a ~^ b)); // simplifying the min terms 
+endmodule
 
+  
